20a35f4d63d8557cc51ec0fb55e295a844a0aa25
Rudra Pratap singh <rpsrathore5388@gmail.com>
2026-09-08 21:07:26 +0530

    Time: 52 ms (36.03%) | Memory: 52.6 MB (88.89%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,43 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const existing = this.cache.get(key);
+    
+    if (existing) {
+        clearTimeout(existing.timer);
+    }
+    
+    const timer = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+    
+    this.cache.set(key, { value, timer });
+    
+    return Boolean(existing);
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        return this.cache.get(key).value;
+    }
+    return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
